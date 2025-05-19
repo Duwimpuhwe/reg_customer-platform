3bb4fe567f980ede60e7c3683cb1195e5c1af882
Duwimpuhwe <duwimpuh@andrew.cmu.edu>
2025-05-19 20:06:01 +0200

    Register CustomerSubmission model to admin

--- customerPrjcts/admin.py
+++ customerPrjcts/admin.py
@@ -1,3 +1,6 @@
 from django.contrib import admin
+from .models import ProjectApplication #added after to register the model
 
 # Register your models here.
+
+admin.site.register(ProjectApplication)
